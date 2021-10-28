d6cb445ff54c9653c25f8c1ac1150b9f67795ed9
MBusto <mikebusto4@gmail.com>
2021-10-28 14:38:43 -0400

    BUGfix: fixed the bug with selecting dates on calender

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -16,7 +16,12 @@ function Header() {
     const selectionRange = {
         startDate: startDate,
         endDate: endDate,
-        key: "Selection"
+        key: "selection"
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
     }
 
     return (
@@ -33,7 +38,7 @@ function Header() {
             </div>
 
             {/* Middle */}
-            <div className='flex items-center md:border-2 md:border-red-400 rounded-full py-2 md:shadow-xl  '>
+            <div className='flex items-center md:border-2 md:border-[#FD5B61] rounded-full py-2 md:shadow-xl  '>
                 <input
                     value={searchInput}
                     onChange={(e) => setSearchInput(e.target.value)}
@@ -41,18 +46,18 @@ function Header() {
                     className='pl-5 bg-transparent outline-none 
                 flex-grow text-sm text-gray-600 placeholder-gray-400 ' />
                 <SearchIcon
-                    className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full 
+                    className='hidden md:inline-flex h-8 bg-[#FD5B61] text-white rounded-full
                     p-2 cursor-pointer md:mx-2'
                 />
             </div>
 
             {/* Right */}
-            <div className='flex space-x-4 items-center justify-end text-red-400'>
+            <div className='flex space-x-4 items-center justify-end text-[#FD5B61]'>
                 <p className='hidden md:inline-flex cursor-pointer'>Become a Host</p>
                 <GlobeAltIcon
                     className='h-6 cursor-pointer'
                 />
-                <div className='flex items-center space-x-2 p-2 rounded-full shadow-xl border-2 border-red-400'>
+                <div className='flex items-center space-x-2 p-2 rounded-full shadow-xl border-2 border-[#FD5B61]'>
                     <MenuIcon className='h-6 cursor-pointer' />
                     <UserCircleIcon className='h-6 cursor-pointer' />
                 </div>
@@ -62,6 +67,9 @@ function Header() {
                 <div>
                     <DateRangePicker
                         ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#FD5B61']}
+                        onChange={handleSelect}
                     />
                 </div>
             }
