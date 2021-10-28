91654b497169f130ae9d13351bd36ba7b78d8b5e
MBusto <mikebusto4@gmail.com>
2021-10-28 15:17:13 -0400

    feat:implemented # of guests

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -11,7 +11,8 @@ function Header() {
 
     const [searchInput, setSearchInput] = useState('')
     const [startDate, setStartDate] = useState(new Date())
-    const [endDate, setEndDate] = useState('')
+    const [endDate, setEndDate] = useState(new Date())
+    const [numOfGuests, setNumOfGuests] = useState(1)
 
     const selectionRange = {
         startDate: startDate,
@@ -64,13 +65,24 @@ function Header() {
             </div>
 
             {searchInput &&
-                <div>
+                <div className='flex flex-col col-span-3 mx-auto'>
                     <DateRangePicker
                         ranges={[selectionRange]}
                         minDate={new Date()}
                         rangeColors={['#FD5B61']}
                         onChange={handleSelect}
                     />
+                    <div className='flex items-center border-b mb-4'>
+                        <h2
+                            className='text-2xl pl-2 flex-grow font-semibold'>Number of Guests
+                        </h2>
+                        <UsersIcon className='h-5' />
+                        <input
+                            value={numOfGuests}
+                            onChange={e => setNumOfGuests(e.target.value)}
+                            type='number'
+                            className='w-12 pl-2 text-lg outline-none text-[#FD5B61]' />
+                    </div>
                 </div>
             }
         </div>
